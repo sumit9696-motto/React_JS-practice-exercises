3d2ac2e056d08264b9d731cff905918c75eb78b7
sumit9696-motto <sumit.rai9696@gmail.com>
2024-11-22 16:33:38 +0530

    new

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,11 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  let counter=2;
+  const addValue=()=>{
+    console.log(counter);
+    counter=counter+1;
+  }
   return (
     <>
       
       <h1>vite+ React</h1>
+      <h2>counter vale : {counter}</h2>
+
+      <button  onClick={addValue}>add to click</button>&nbsp;
+
+      <button>remove to click </button>
       
       
     </>
